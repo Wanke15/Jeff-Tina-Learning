0f791c1d13473427f9b1f8b83226c24f9ee8cd72
jeff <jeff.wang@ribitwo.com>
2019-09-12 14:11:14 +0800

    add statsWithMissing function

--- 2019/09/Jeff/LinkageAnalysis/src/main/scala/demo.scala
+++ 2019/09/Jeff/LinkageAnalysis/src/main/scala/demo.scala
@@ -3,10 +3,25 @@ import org.apache.spark.util.StatCounter
 object demo {
   def main(args: Array[String]): Unit = {
     val statCounter = new StatCounter()
-    println(statCounter.toString)
+    //println(statCounter.toString)
 
     statCounter.merge(1)
     statCounter.merge(5)
-    println(statCounter.toString)
+    //println(statCounter.toString)
+
+    val nas3 = Array(1.0, Double.NaN).map(d => NAStatCounter(d))
+    val nas4 = Array(Double.NaN, 2.0).map(d => NAStatCounter(d))
+
+    nas3.foreach(println)
+    println("*************")
+    nas4.foreach(println)
+
+    nas3.zip(nas4).foreach(println)
+    println("##############")
+    val merged = nas3.zip(nas4).map(p => p._1.merge(p._2))
+    merged.foreach(println)
+    println("@@@@@@@@@@@@@@@")
+    val merged2 = nas3.zip(nas4).map {case (a, b) => a.toString + "; " + b.toString }
+    merged2.foreach(println)
   }
 }
--- 2019/09/Jeff/LinkageAnalysis/src/main/scala/linkageAnalysis2.scala
+++ 2019/09/Jeff/LinkageAnalysis/src/main/scala/linkageAnalysis2.scala
@@ -12,6 +12,8 @@ object linkageAnalysis2 {
     val noheader = rawblocks.filter(x => !isHeader(x))
     val parsed = noheader.map(line => parse(line))
 
+    println(parsed.count())
+
     val nas1 = NAStatCounter(10.0)
     nas1.add(2.1)
     // println(nas1.toString())
@@ -25,12 +27,22 @@ object linkageAnalysis2 {
 
     //nas.foreach(println)
 
-    val nasRDD = parsed.map(md => {md.scores.map(d => NAStatCounter(d))})
+    val nas3 = Array(1.0, Double.NaN).map(d => NAStatCounter(d))
+    val nas4 = Array(Double.NaN, 2.0).map(d => NAStatCounter(d))
 
-    nasRDD.take(2).foreach(x => x.foreach(println))
+    val merged = nas3.zip(nas4)
+
+    val nasRDD = parsed.map(md => {md.scores.map(d => NAStatCounter(d))}).cache()
+
+    nasRDD.take(1).foreach(x => x.foreach(println))
+
+    println(nasRDD.count())
 
     val reduced = nasRDD.reduce((n1, n2) => {n1.zip(n2).map { case (a, b) => a.merge(b)}})
-    reduced.take(2).foreach(println)
+
+    println("************")
+    reduced.foreach(println)
+    println("############")
 
   }
 }
